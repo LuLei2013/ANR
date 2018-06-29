6977d1ad65ee6a2d43041389e602d6bd92de0240
lulei <lulei@didichuxing.com>
2018-06-29 15:54:31 +0800

    finish

--- app/src/main/java/com/ll/anr/anr/TestServiceActivity.java
+++ app/src/main/java/com/ll/anr/anr/TestServiceActivity.java
@@ -1,48 +1,71 @@
 package com.ll.anr.anr;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
-import static android.content.Intent.FLAG_RECEIVER_FOREGROUND;
+import com.ll.anr.anr.service.MyService;
+
 
 public class TestServiceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.hello).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        setContentView(R.layout.test_service);
 
-                Intent intent = new Intent("com.anr.ll");
-                intent.addFlags(FLAG_RECEIVER_FOREGROUND);
-                sendBroadcast(intent);
-            }
-        });
-
-
-//        findViewById(R.id.world).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, MyService.class);
-//                bindService(intent, new ServiceConnection() {
-//                    @Override
-//                    public void onServiceConnected(ComponentName name, IBinder service) {
-//                        Log.e("Ruby","onServiceConnected");
-//                    }
-//
-//                    @Override
-//                    public void onServiceDisconnected(ComponentName name) {
-//
-//                    }
-//
-//                }, BIND_AUTO_CREATE);
-//            }
-//        });
+        findViewById(R.id.start_service).setOnClickListener(mOnclickListener);
+        findViewById(R.id.stop_service).setOnClickListener(mOnclickListener);
+        findViewById(R.id.bind_service).setOnClickListener(mOnclickListener);
+        findViewById(R.id.unbind_service).setOnClickListener(mOnclickListener);
     }
 
 
+    private View.OnClickListener mOnclickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(TestServiceActivity.this, MyService.class);
+            switch (v.getId()) {
+                case R.id.start_service:
+                    startService(intent);
+                    break;
+                case R.id.stop_service:
+                    stopService(intent);
+                    break;
+                case R.id.bind_service:
+                    bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+                    break;
+                case R.id.unbind_service:
+                    unbindService(serviceConnection);
+                    break;
+            }
+        }
+    };
+
+    ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.e("Ruby", "onServiceConnected start");
+            sleep30();
+            Log.e("Ruby", "onServiceConnected end");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    private void sleep30() {
+        try {
+            Thread.sleep(30 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/ll/anr/anr/service/MyService.java
+++ app/src/main/java/com/ll/anr/anr/service/MyService.java
@@ -1,45 +1,93 @@
 package com.ll.anr.anr.service;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.ll.anr.anr.MainActivity;
+import com.ll.anr.anr.R;
+
 public class MyService extends Service {
+
+
+    final static private int SECOND = 1000;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
 
-        Log.e("Ruby","onBind");
+        Log.e("Ruby", "onBind start");
+        sleep30();
+        Log.e("Ruby", "onBind end");
         return null;
 
     }
 
     @Override
     public void onCreate() {
-        Log.e("Ruby","onCreate");
+        setForeGroundService();
+        Log.e("Ruby", "onCreate start");
+        sleep30();
+        Log.e("Ruby", "onCreate end");
         super.onCreate();
     }
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.e("Ruby","onStartCommand");
-
+        Log.e("Ruby", "onStartCommand start");
+        sleep30();
+        Log.e("Ruby", "onStartCommand end");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        Log.e("Ruby","onUnbind");
+        Log.e("Ruby", "onUnbind start");
+        sleep30();
+        Log.e("Ruby", "onUnbind end");
         return super.onUnbind(intent);
     }
 
 
     @Override
     public void onDestroy() {
-        Log.e("Ruby","onDestroy");
+        Log.e("Ruby", "onDestroy start");
+        sleep30();
+        Log.e("Ruby", "onDestroy end");
         super.onDestroy();
+    }
+
+    private void sleep30() {
+        try {
+            Thread.sleep(40 * SECOND);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setForeGroundService() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("通知")
+                .setContentText("前台Service");
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        mBuilder.setContentIntent(resultPendingIntent);
+        Notification notification = mBuilder.build();
+        notification.defaults = Notification.DEFAULT_SOUND;
+        //前台 service
+        startForeground(1, notification);
     }
 }
